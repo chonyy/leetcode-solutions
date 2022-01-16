4512300eaf6ed48bbfcc16fe28c0d8d0e5301db1
Tony Chou <tony.chou@intel.com>
2022-01-16 18:21:50 +0800

    Time: 42 ms (72.04%), Space: 8.1 MB (82.59%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    void remove(string& s, int l, int r, vector<string>& res, int idx) {
+        if(l == 0 && r == 0) {
+            // cout << s << endl;
+            if(isValid(s)) {
+                res.push_back(s);
+            }
+            return;
+        }
+        
+        int n = s.size();
+        for(int i = idx; i < n; i ++) {
+            if(i > idx && s[i] == s[i-1])
+                continue;
+            
+            char c = s[i];
+            string sub = s.substr(0, i) + s.substr(i+1);
+            if(c == '(') {
+                remove(sub, l-1, r, res, i);
+            }
+            if(c == ')') {
+                remove(sub, l, r-1, res, i);
+            }
+        }
+    }
+    
+    vector<string> removeInvalidParentheses(string s) {
+        int l = 0;
+        int r = 0;
+        int remainLeft = 0;
+        
+        for(char c : s) {
+            if(c == '(') {
+                remainLeft ++;
+            }
+            if(c == ')') {
+                if(remainLeft == 0)
+                    r ++;
+                else
+                    remainLeft --;
+            }
+        }
+        
+        l = remainLeft;
+        // cout << l << " " << r << endl;
+        
+        vector<string> res;
+        remove(s, l, r, res, 0);
+        return res;
+        
+    }
+    
+    
+    bool isValid(string& s) {
+        int leftCount = 0;
+        
+        for(char c : s) {
+            if(c == '(') {
+                leftCount ++;
+            }
+            if(c == ')') {
+                leftCount --;
+                if(leftCount < 0)
+                    return false;
+            }
+        }
+        
+        return leftCount == 0;
+    }
+};
+
