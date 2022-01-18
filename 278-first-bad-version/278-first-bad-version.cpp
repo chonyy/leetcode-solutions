e702919694f437969cfb1a02a89d8577bcf9ecc2
Tony Chou <tony.chou@intel.com>
2022-01-18 16:48:42 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (21.68%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 0;
+        int r = n;
+        
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(isBadVersion(mid)) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        
+        return l;
+    }
+};
