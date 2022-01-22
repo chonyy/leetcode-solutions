192339634f15abf871d2e76644e3949709ae62c6
Tony Chou <tony.chou@intel.com>
2022-01-22 18:12:14 +0800

    Time: 213 ms (51.28%), Space: 13.8 MB (83.18%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -3,23 +3,21 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> words(wordList.begin(), wordList.end());
         queue<string> q;
-        unordered_set<string> visited;
         
         if(words.find(endWord) == words.end()) 
             return 0;
         
         q.push(beginWord);
-        visited.insert(beginWord);
         int steps = 1;
         
         while(!q.empty()) {
             int size = q.size();
             // cout << steps << endl;
-            for(int i = 0; i < size; i ++) {
+            for(int i = 0; i < size; i ++) { 
                 auto cur = q.front();
                 q.pop();
                 
-                // Try all possible transformation
+                // Try all possible transformation M
                 for(int j = 0; j < cur.size(); j ++) {
                     string candidate = cur;
                     for(char c = 'a'; c <= 'z'; c ++) {
@@ -31,16 +29,12 @@ public:
                             return steps+1;
                         }
                         
-                        if(visited.find(candidate) != visited.end()) {
-                            continue;
-                        }
-                        
                         if(words.find(candidate) == words.end()) {
                             continue;
                         }
                         
                         // cout << candidate << endl;
-                        visited.insert(candidate);
+                        words.erase(candidate);
                         q.push(candidate);
                     }
                 }
