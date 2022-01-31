b74664d0102e97ba5c1aea554bd0cbecb5964842
Tony Chou <tony.chou@intel.com>
2022-02-01 00:09:54 +0800

    Time: 408 ms (13.65%), Space: 12 MB (41.52%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int target, int idx, vector<vector<int>>& memo) {
-        if(target < 0)
-            return false;
-        if(target == 0)
-            return true;
-        if(idx == nums.size()) 
-            return false;
-        if(memo[idx][target] != -1)
-            return memo[idx][target];
-        
-        bool res = solve(nums, target-nums[idx], idx+1, memo) || solve(nums, target, idx+1, memo);
-        
-        return memo[idx][target] = res;
-    }
-    
-    
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
-        
-        
         for(int num : nums) {
             sum += num;
         }
         int target = sum / 2;
         if(sum % 2)
             return false;
-        vector<vector<int>> memo(n+1, vector<int>(target+1, -1));
+        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        dp[0][0] = true;
         
-        return solve(nums, target, 0, memo);
+        for(int i = 1; i <= n; i ++) {
+            int num = nums[i-1];
+            for(int j = 1; j <= target; j ++) {
+                int need = j - num;
+                dp[i][j] = dp[i-1][j];
+                if(need >= 0) {
+                    dp[i][j] = dp[i][j] | dp[i-1][need];
+                }
+            }
+        }
+        
+        return dp[n][target];
     }
 };
