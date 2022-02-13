ff6e293fcc5338ee40043289636a137d8b226c8e
Tony Chou <tony.chou@intel.com>
2022-02-13 16:08:59 +0800

    Time: 124 ms (85.75%), Space: 80.7 MB (6.02%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,26 +5,20 @@ public:
         vector<int> lessRight(n, n);
         vector<int> lessLeft(n, -1);
         
-        stack<int> stk;
-        
-        for(int i = 0; i < n; i ++) {
-            int h = heights[i];
-            while(!stk.empty() && h < heights[stk.top()]) {
-                lessRight[stk.top()] = i;
-                stk.pop();
+        for(int i = n-2; i >= 0; i --) {
+            int p = i + 1;
+            while(p < n && heights[p] > heights[i]) {
+                p = lessRight[p];
             }
-            stk.push(i);
+            lessRight[i] = p;
         }
         
-        stk = {};
-        
-        for(int i = n-1; i >= 0; i --) {
-            int h = heights[i];
-            while(!stk.empty() && h < heights[stk.top()]) {
-                lessLeft[stk.top()] = i;
-                stk.pop();
+        for(int i = 1; i < n; i ++) {
+            int p = i - 1;
+            while(p >= 0 && heights[p] >= heights[i]) {
+                p = lessLeft[p];
             }
-            stk.push(i);
+            lessLeft[i] = p;
         }
         
         int maxArea = 0;
