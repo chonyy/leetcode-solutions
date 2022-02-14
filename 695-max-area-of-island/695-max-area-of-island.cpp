efb637d29ec7f927c168b996ca32ddca738b8445
Tony Chou <tony.chou@intel.com>
2022-02-14 17:07:18 +0800

    Time: 42 ms (22.76%), Space: 27.1 MB (13.18%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
-    int rows;
-int cols;
-
-int getArea(vector<vector<int>>& grid, int row, int col) {
-	if(row < 0 || row >= rows || col < 0 || col >= cols)
-		return 0;
-
-	if(grid[row][col] == 0)
-		return 0;
-
-	grid[row][col] = 0;
-	int area = 0;
-	area += getArea(grid, row+1, col);
-	area += getArea(grid, row-1, col);
-	area += getArea(grid, row, col+1);
-	area += getArea(grid, row, col-1);
-	return area + 1;
-}
-
-int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int rows = 0;
+    int cols = 0;
+    vector<vector<int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    
+    int getArea(vector<vector<int>>& grid, int row, int col) {
+        stack<pair<int,int>> stk;
+        stk.push({row, col});
+        grid[row][col] = 0;
+        int area = 1;
+        
+        while(!stk.empty()) {
+            auto cur = stk.top();
+            stk.pop();
+            
+            for(auto& dir : dirs) {
+                int newRow = cur.first + dir[0];
+                int newCol = cur.second + dir[1];
+                
+                if(newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols)
+                    continue;
+                if(grid[newRow][newCol] == 0)
+                    continue;
+                
+                grid[newRow][newCol] = 0;
+                stk.push({newRow, newCol});
+                area ++;
+            }
+        }
+        
+        return area;
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         rows = grid.size();
         cols = grid[0].size();
-int ans = 0;        
-
+        int ans = 0;
+        
         for(int i = 0; i < rows; i ++) {
             for(int j = 0; j < cols; j ++) {
-		if(grid[i][j] == 0) 
-			continue;
-	ans = max(ans, getArea(grid, i, j));
-}
+                if(grid[i][j] == 0)
+                    continue;
+                int area = getArea(grid, i, j);
+                ans = max(ans, area);
+            }
         }
-
-	return ans;
-   }
-
+        
+        return ans;
+    }
 };
