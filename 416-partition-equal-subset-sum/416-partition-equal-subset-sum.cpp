3441494c7e88739b23733b5785e8d8e4198c297d
Tony Chou <tony.chou@intel.com>
2022-02-24 03:38:42 +0800

    Time: 314 ms (32.30%), Space: 75.4 MB (19.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        int n = nums.size();
-        for(int num : nums) {
-            sum += num;
-        }
-        int target = sum / 2;
-        if(sum % 2)
-            return false;
-        vector<bool> dp(target+1, false);
-        dp[0] = true;
-        
-        for(int i = 1; i <= n; i ++) {
-            int num = nums[i-1];
-            vector<bool> temp(target+1, false);
-            for(int j = 1; j <= target; j ++) {
-                int need = j - num;
-                temp[j] = dp[j];
-                if(need >= 0) {
-                    temp[j] = temp[j] | dp[need];
-                }
-            }
-            swap(dp, temp);
-        }
-        
-        return dp[target];
-    }
+    bool checkPartition(vector<int>& nums, int idx, int target, vector<vector<int>>& memo) {
+	// ending condition
+	if(target == 0)
+		return true;
+	if(idx == nums.size() || target < 0)
+		return false;
+
+	if(memo[idx][target] != -1)
+		return memo[idx][target];
+
+	bool take = checkPartition(nums, idx+1, target - nums[idx], memo);
+	bool notTake = checkPartition(nums, idx+1, target, memo);
+	bool res = take || notTake;
+
+	return memo[idx][target] = res;
+}
+
+bool canPartition(vector<int>& nums) {
+int sum = 0;
+for(int num : nums) {
+	sum += num;
+}        
+
+if(sum % 2) {
+	return false;
+}
+
+int n = nums.size();
+int target = sum / 2;
+vector<vector<int>> memo(n, vector<int>(target+1, -1));
+
+
+return checkPartition(nums, 0, target, memo);
+}
+
+
+
 };
