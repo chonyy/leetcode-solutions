fb46f5512d4a42cbadf51f0dfc1df2cb5133d86f
Tony Chou <tony.chou@intel.com>
2022-03-06 05:08:04 +0800

    Time: 129 ms (94.70%), Space: 41.9 MB (81.17%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.cpp
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     bool differByOne(vector<string>& dict) {
-        int n = dict.size();
-        int len = dict[0].size();
-        
-        for(int i = 0; i < len; i ++) {
-            unordered_set<string> seen;
-            for(auto& str : dict) {
-                string sub = str.substr(0,i) + str.substr(i+1);
-                if(seen.count(sub))
-                    return true;
-                seen.insert(sub);
-            }
+        const int MOD = 1'000'000'007; 
+        vector<int> hs; 
+        for (auto& word : dict) {
+            long val = 0; 
+            for (auto& ch : word) val = (26*val + ch - 'a') % MOD; 
+            hs.push_back(val); 
         }
         
-        return false;
+        int m = dict.size(), n = dict[0].size(); 
+        long mult = 1l; 
+        for (int j = n-1; j >= 0; --j) {
+            unordered_map<int, vector<int>> seen; 
+            for (int i = 0; i < m; ++i) {
+                int val = (hs[i] - (dict[i][j] - 'a')*mult + 26l*MOD) % MOD; 
+                if (seen.count(val)) 
+                    for (auto& ii : seen[val]) 
+                        if (dict[ii].substr(0, j) == dict[i].substr(0, j) && dict[ii].substr(j+1, n-j-1) == dict[i].substr(j+1, n-j-1)) return true; 
+                seen[val].push_back(i); 
+            }
+            mult = (26 * mult) % MOD; 
+        }
+        return false; 
     }
 };
