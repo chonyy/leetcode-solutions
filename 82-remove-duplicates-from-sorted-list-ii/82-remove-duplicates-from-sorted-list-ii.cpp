e66d7df66ffaa2d472628b458e4c397183baefb1
Tony Chou <tony.chou@intel.com>
2022-03-10 00:56:54 +0800

    Time: 0 ms (100.00%), Space: 11.2 MB (20.51%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -18,23 +18,26 @@ public:
         while(cur) {
             bool same = false;
             
+            // skip all the same num, set the flag to true if there's any same
             while(cur && cur->next) {
                 if(cur->val != cur->next->val) {
                     break;
                 }
-                
                 same = true;
                 cur = cur->next;
             }
             
+            // append to result if there's no same
             if(!same) {
                 resHead->next = cur;
                 resHead = resHead->next;
             }
             
+            // no matter there's same or not, we always want to advance one
             cur = cur->next;
         }
         
+        // make sure the we are pointing to NULL at last
         resHead->next = nullptr;
         return sentinel->next;
     }
