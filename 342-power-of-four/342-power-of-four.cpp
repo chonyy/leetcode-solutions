f217a94493e9f65f84389cca91ff9887990c72cc
Tony Chou <tony.chou@intel.com>
2022-03-11 02:32:43 +0800

    Time: 0 ms (100.00%), Space: 5.8 MB (88.66%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n <= 0)
+            return false;
+        
+        while(n > 1) {
+            if(n % 4)
+                return false;
+            n = n / 4;
+        }
+        
+        return true;
+    }
+};
