bcbc94b1e6fac720d8e8bf7032e489c732ec3790
Tony Chou <tony.chou@intel.com>
2022-03-15 18:58:32 +0800

    Time: 1009 ms (38.89%), Space: 181.8 MB (80.95%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(int cur, int parent, unordered_map<int, vector<int>>& graph, unordered_map<char,int>& count, vector<int>& res, string& labels) {
+        char lab = labels[cur];
+        int oldCount = count[lab];
+        count[lab] ++;
+        
+        auto& neighbors = graph[cur];
+        for(auto& nei : neighbors) {
+            if(nei == parent) {
+                continue;
+            }
+            dfs(nei, cur, graph, count, res, labels);
+        }
+        
+        res[cur] = count[lab] - oldCount;
+    }
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        unordered_map<int, vector<int>> graph;
+        
+        for(auto& e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        
+        vector<int> res(n);
+        unordered_map<char,int> count;
+        dfs(0, -1, graph, count, res, labels);
+        
+        return res;
+    }
+};
