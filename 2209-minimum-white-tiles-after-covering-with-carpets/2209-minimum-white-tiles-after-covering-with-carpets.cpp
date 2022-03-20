bff1061bde44484f9db34e3e3fe8991532355d05
Tony Chou <tony.chou@intel.com>
2022-03-20 16:57:28 +0800

    Time: 1244 ms (11.11%), Space: 254.4 MB (11.11%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int solve(int idx, int used, string& floor, int numCarpets, int carpetLen, vector<int>& whites, vector<vector<int>>& mem) {
-        if(idx >= floor.size()) {
-            return 0;
-        }
-        
-        if(used == numCarpets) {
-            return whites[idx];
-        }
-        
-        if(mem[idx][used] != -1) {
-            return mem[idx][used];
-        }
-        
-        int cover = solve(idx+carpetLen, used+1, floor, numCarpets, carpetLen, whites, mem);
-        int noCover = (floor[idx] == '1') + solve(idx+1, used, floor, numCarpets, carpetLen, whites, mem);
-        
-        return mem[idx][used] = min(cover, noCover);
-    }
-    
     int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
         int n = floor.size();
-        vector<int> whites(n, 0);
-        vector<vector<int>> mem(n, vector<int>(numCarpets, -1));
+        vector<vector<int>> dp(n+1, vector<int>(numCarpets+1));
         
-        whites[n-1] = floor[n-1] == '1';
-        for(int i = n-2; i >= 0 ; i --) {
-            whites[i] = (floor[i] == '1') + whites[i+1];
+        for(int i = 1; i <= n; i ++) {
+            for(int j = 0; j <= numCarpets; j ++) {
+                if(j == 0) {
+                    dp[i][j] = (floor[i-1] == '1') + dp[i-1][j];
+                    continue;
+                }
+                
+                int noCover = (floor[i-1] == '1') + dp[i-1][j];
+                int cover = dp[max(i - carpetLen, 0)][j-1];
+                dp[i][j] = min(cover, noCover);
+            }
         }
         
-        return solve(0, 0, floor, numCarpets, carpetLen, whites, mem);
+        return dp[n][numCarpets];
     }
+    
 };
+
+// dp[i][j] = use j carpet to i idx => min white tiles 
