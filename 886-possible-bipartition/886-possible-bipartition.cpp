32f04f41282db821cc27c4395bd8f42c3f57b73a
Tony Chou <tony.chou@intel.com>
2022-03-27 18:06:29 +0800

    Time: 237 ms (70.85%), Space: 67.3 MB (50.54%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool coloring(unordered_map<int,vector<int>>& graph, int node, int curColor, vector<int>& color) {
+        color[node] = curColor;
+        // cout << "node " << node << " color " << curColor << endl;
+        auto& neighbors = graph[node];
+        
+        for(auto& nei : neighbors) {
+            if(color[nei] == curColor) {
+                cout << nei << " color " << curColor << endl;
+                return false;
+            }
+            if(color[nei] == 0 && coloring(graph, nei, -curColor, color) == false) {
+                // cout << node << " -> " << nei << endl;
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int, vector<int>> graph;
+        for(auto& e : dislikes) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        vector<int> color(n+1, 0);
+        
+        for(int i = 1; i <= n; i ++) {
+            if(color[i] != 0) {
+                continue;
+            }
+            if(coloring(graph, i, 1, color) == false) {
+                cout << i << endl;
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
