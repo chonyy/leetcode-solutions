c3fadff35bfedeea6110264f1f03de0d2e7e9680
Tony Chou <tony.chou@intel.com>
2022-04-25 19:16:28 +0800

    Time: 3 ms (90.82%), Space: 58.5 MB (7.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,6 +1,7 @@
 class Solution {
     private int rows;
     private int cols;
+    int[][] dirs = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
     
     public void dfs(char[][] grid, int row, int col) {
         if(row < 0 || row >= rows || col < 0 || col >= cols) {
@@ -15,6 +16,32 @@ class Solution {
         dfs(grid, row-1, col);
         dfs(grid, row, col+1);
         dfs(grid, row, col-1);
+    }
+    
+    public void bfs(char[][] grid, int row, int col) {
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        q.offer(new Pair(row, col));
+        
+        while(q.size() > 0) {
+            var cur = q.poll();
+            int curRow = cur.getKey();
+            int curCol = cur.getValue();
+            
+            for(var dir : dirs) {
+                int newRow = curRow + dir[0];
+                int newCol = curCol + dir[1];
+                
+                if(newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
+                    continue;
+                }
+                if(grid[newRow][newCol] == '0') {
+                    continue;
+                }
+                
+                grid[newRow][newCol] = '0';
+                q.offer(new Pair(newRow, newCol));
+            }
+        }
     }
     
     public int numIslands(char[][] grid) {
