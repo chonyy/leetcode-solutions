26667669f281d006e5b8977c2886241eccb3e0f6
Tony Chou <tony.chou@intel.com>
2022-06-12 04:11:39 +0800

    Time: 281 ms (57.93%), Space: 98.4 MB (98.52%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        
+        int l = 0;
+        int r = 0;
         int n = nums.size();
-        unordered_map<int,int> seen;
         int curSum = 0;
         int ans = INT_MAX;
         
-        for(int i = 0; i < n; i ++) {
-            curSum += nums[i];
-            seen[curSum] = i;
-            if(curSum == x) {
-                ans = i +1;
-                cout << "set" << endl;
+        while(r < n) {
+            curSum += nums[r];
+            while(l <= r && curSum > sum - x) {
+                curSum -= nums[l];
+                    l ++;
             }
-        }
-        
-        curSum = 0;
-        
-        seen[0] = -1;
-        for(int i = n-1; i >= 0; i --) {
-            curSum += nums[i];
-            int target = x - curSum;
-            if(seen.find(target) != seen.end()) {
-                if(seen[target] >= i)
-                    continue;
-                ans = min(ans, n - i + seen[target] +1);
+            if(curSum == sum - x) {
+                ans = min(ans, n - (r - l + 1));
             }
+            r ++;
         }
         
         return ans == INT_MAX ? -1 : ans;
