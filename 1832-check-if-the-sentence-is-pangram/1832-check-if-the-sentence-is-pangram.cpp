e8af33a316ac16da1981379240434ec126ae4fb5
Tony Chou <tony.chou@intel.com>
2022-10-17 22:10:44 +0100

    Time: 3 ms (80.72%), Space: 7.2 MB (6.62%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        // Array 'seen' of size 26.
-        array<bool, 26> seen{};
-
-        // For every letter 'currChar', we find its ASCII code, 
-        // and update value at the mapped index as true.
-        for (auto currChar : sentence)
-            seen[currChar - 'a'] = true;
+        // Add every letter of 'sentence' to hash set 'seen'.
+        unordered_set<char> seen(sentence.begin(), sentence.end());
         
-        // Once we finish iterating, check if 'seen' contains false.
-        for (auto status : seen)
-            if (!status)
-                return false;
-        return true;
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
     }
 };
