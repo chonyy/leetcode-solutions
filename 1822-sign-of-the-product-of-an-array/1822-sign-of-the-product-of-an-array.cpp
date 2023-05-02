e2453eb3387a2e5225a9401d36a6dfaa732c0b9b
Tony Chou <tony.chou@intel.com>
2023-05-03 00:00:57 +0100

    Time: 0 ms (100.00%), Space: 10.1 MB (85.57%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+        
+        for(int num : nums) {
+            if(num == 0) {
+                return 0;
+            }
+            if(num < 0) {
+                sign = -sign;
+            }
+        }
+        
+        return sign;
+    }
+};
