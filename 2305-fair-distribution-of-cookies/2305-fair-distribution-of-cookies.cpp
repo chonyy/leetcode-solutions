167f2042cb999e914423800dc07adef257ed7c3e
Tony Chou <tony.chou@intel.com>
2023-07-03 00:12:04 +0100

    Time: 2393 ms (5.48%), Space: 6.8 MB (50.51%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ans = INT_MAX;
+    void solve(int start, vector<int>& nums, vector<int>& v, int k){
+        if(start==nums.size()){
+            int maxm = INT_MIN;
+            for(int i=0;i<k;i++){
+                maxm = max(maxm,v[i]);
+            }
+            ans = min(ans,maxm);
+            return;
+        }
+        for(int i=0;i<k;i++){
+            v[i] += nums[start];
+            solve(start+1,nums,v,k);
+            v[i] -= nums[start];
+        }
+    }
+    
+    int distributeCookies(vector<int>& nums, int k) { // nums is the cookies vector
+        int n = nums.size();
+        vector<int> v(k,0); // v is to store each sum of the k subsets
+        solve(0,nums,v,k);
+        return ans;
+    }
+};
