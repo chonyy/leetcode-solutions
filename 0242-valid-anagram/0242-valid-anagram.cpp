65d9dfc834884e0900ee74936e327185feef0054
Tony Chou <tony.chou@intel.com>
2023-12-19 02:15:36 +0800

    Time: 10 ms (49.30%), Space: 7.7 MB (43.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) {
+            return false;
+        }
+        
+        unordered_map<char, int> m;
+        
+        for(char c : s) {
+            m[c] ++;
+        }
+        
+        for(char c : t) {
+            if(-- m[c] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
