c68551a7c19c440476b1c265d74a739e736eb4c0
Tony Chou <tony.chou@intel.com>
2024-02-04 19:55:58 +0000

    Time: 7 ms (80.60%), Space: 10.8 MB (64.89%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& A, int K) {
+        int N = A.size();
+        vector<int> dp(N + 1);
+        for (int i = 1; i <= N; ++i) {
+            int curMax = 0, best = 0;
+            for (int k = 1; k <= K && i - k >= 0; ++k) {
+                curMax = max(curMax, A[i - k]);
+                best = max(best, dp[i - k] + curMax * k);
+            }
+            dp[i] = best; 
+        }
+        return dp[N];
+    }
+};
