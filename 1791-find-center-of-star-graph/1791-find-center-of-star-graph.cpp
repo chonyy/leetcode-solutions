caccfddb9396243d9a1461784d7c8531bf02c066
Tony Chou <tony.chou@intel.com>
2024-06-27 01:24:55 +0100

    Time: 148 ms (40.47%), Space: 70.6 MB (98.95%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -4,6 +4,7 @@ public:
         auto& firstEdge = edges[0];
         auto& secondEdge = edges[1];
         
-        return firstEdge[0] == secondEdge[0] ? firstEdge[0] : (firstEdge[0] == secondEdge[1] ? firstEdge[0] : firstEdge[1]);
+        return firstEdge[0] == secondEdge[0] ? firstEdge[0] :
+        (firstEdge[0] == secondEdge[1] ? firstEdge[0] : firstEdge[1]);
     }
 };
