2e970abd43b3ac11b8ffe5e759460bed37e43a03
Tony Chou <tony.chou@intel.com>
2024-08-18 17:05:29 +0100

    Time: 3 ms (76.61%), Space: 9.5 MB (76.75%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        unordered_set<long long> seen;
-        pq.push(1);
+        int i2 = 0, i3 = 0, i5 = 0;
+        vector<int> nums(n, 1);
+        
+        for(int i = 1; i < n; i ++) {
+            int nextNum = min(
+                min(nums[i2] * 2, nums[i3] * 3),
+                nums[i5] * 5
+            );
+            nums[i] = nextNum;
+            // cout << nextNum << endl;
+            // cout << i2 << " " << i3 << " " << i5 << endl;
 
-        int i = 0;
+            // cout << nums[i2] * i2 << endl;
 
-        while(i <= n) {
-            long long num = pq.top();
-            pq.pop();
-
-            if(seen.count(num)) {
-                continue;
-            }
-
-            seen.insert(num);
-            cout << num << endl;
-
-            pq.push(num * 2);
-            pq.push(num * 3);
-            pq.push(num * 5);
-
-            if(i == n-1)
-                return num;
-
-            i ++;
+            if(nextNum == (nums[i2] * 2))
+                i2 ++;
+            if(nextNum == (nums[i3] * 3))
+                i3 ++;
+            if(nextNum == (nums[i5] * 5))
+                i5 ++;
         }
 
-        return -1;
-        
+        return nums[n-1];
     }
 };
