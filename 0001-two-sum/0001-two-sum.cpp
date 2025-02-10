947688c915ef908e2ee10504b5ba6495e2bbb164
Tony Chou <tony.chou@intel.com>
2025-02-10 03:27:03 +0000

    Time: 0 ms (100%), Space: 14.8 MB (35.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> cache;
+        unordered_map<int,int> m;
         int n = nums.size();
-        
+
         for(int i = 0; i < n; i ++) {
             int num = nums[i];
-            if(cache.contains(num)) {
-                return {cache[num], i};
+
+            if(m.count(num)) {
+                return {m[num], i};
             }
-            
-            cache[target - num] = i;
+
+            int diff = target - num;
+            m[diff] = i;
         }
-        
+
         return {};
     }
 };
