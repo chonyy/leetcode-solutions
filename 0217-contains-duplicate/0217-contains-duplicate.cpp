9ddec89954a93f8e1f449d7d7258279fbfeae1c7
Tony Chou <tony.chou@intel.com>
2026-01-21 10:41:13 -0800

    Time: 50 ms (54.41%), Space: 90.9 MB (44.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,10 +3,9 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
 
-        for(int num : nums) {
-            if(seen.count(num)) {
+        for (int num : nums) {
+            if(seen.count(num))
                 return true;
-            }
 
             seen.insert(num);
         }
