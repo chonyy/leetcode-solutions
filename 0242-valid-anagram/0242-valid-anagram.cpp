c139cafaf414feee1f9112cf4dd1e40ba44d59e7
Tony Chou <tony.chou@intel.com>
2026-01-21 10:38:55 -0800

    Time: 0 ms (100%), Space: 9.8 MB (48.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> m;
+        unordered_map<char,int> count;
 
-        if(s.size() != t.size()) {
+        if (s.size() != t.size()) {
             return false;
         }
 
-        for(char c : s) {
-            m[c] ++;
+        for (char c : s) {
+            count[c] ++;
         }
 
-        for(char c : t) {
-            if(m[c] == 0) {
+        for (char c : t) {
+            count[c] --;
+
+            if (count[c] < 0) {
                 return false;
             }
-
-            m[c] --;
         }
 
         return true;
