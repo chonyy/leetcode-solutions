55a3702daa0cbb5adf35f4373f8a0986e6c6b7a5
Tony Chou <tony.chou@intel.com>
2026-01-22 03:36:45 -0800

    Time: 3 ms (38.19%), Space: 42.3 MB (16.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> leftProd(n, 1);
-        vector<int> rightProd(n, 1);
-        vector<int> res;
-        
-        for(int i = 1; i < n; i ++) {
-            int ri = (n-1) - i;
-            leftProd[i] = nums[i-1] * leftProd[i-1];
-            rightProd[ri] = nums[ri+1] * rightProd[ri+1];
+        vector<int> prefixSum(n);
+        vector<int> suffixSum(n);
+
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i ++) {
+            prefixSum[i] = nums[i] * prefixSum[i-1];
         }
-        
-        for(int i = 0; i < n ; i++) {
-            // cout << leftProd[i] << " " << rightProd[i] << endl;
-            res.push_back(leftProd[i] * rightProd[i]);
+
+        suffixSum[n-1] = nums[n-1];
+        for (int i = n-2; i >= 0; i --) {
+            suffixSum[i] = nums[i] * suffixSum[i+1];
         }
-        
+
+        vector<int> res(n);
+        res[0] = suffixSum[1];
+        res[n-1] = prefixSum[n-2];
+
+        for (int i = 1; i < n-1; i ++) {
+            res[i] = prefixSum[i-1] * suffixSum[i+1];
+        }
+
         return res;
     }
 };
 
-
 // 1 2 3 4
-// 24 12 4 1    
-// 1 1 2 6  
+// 1 2 6 24
+// 24 24 12 4
