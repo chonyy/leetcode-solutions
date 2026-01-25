924523c67381fa28208bb8298d85db789d63de36
Tony Chou <tony.chou@intel.com>
2026-01-24 16:22:52 -0800

    Time: 126 ms (13.7%), Space: 91.2 MB (12.11%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> set;
-
-        for (int num : nums) {
-            set.insert(num);
-        }
-
+        unordered_map<int,int> curLen;
         int res = 0;
 
-        for (int num : set) {
-            if (set.contains(num+1)) {
+        for (int num : nums) {
+            if (curLen[num]) {
                 continue;
             }
 
-            int cur = num;
-            int streak = 1;
-            while (set.contains(cur-1)) {
-                streak ++;
-                cur --;
-            }
+            curLen[num] = curLen[num-1] + curLen[num+1] + 1;
+            curLen[num - curLen[num-1]] = curLen[num];
+            curLen[num + curLen[num+1]] = curLen[num];
 
-            res = max(res, streak);
+            // cout << num << " " << curLen[num] << endl;
+
+            res = max(res, curLen[num]);   
         }
 
         return res;
