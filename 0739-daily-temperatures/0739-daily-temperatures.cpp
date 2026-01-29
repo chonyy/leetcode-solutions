2a5760b9845f663dcb400e80b5ca28049176c939
Tony Chou <tony.chou@intel.com>
2026-01-29 02:13:22 -0800

    Time: 7 ms (97.71%), Space: 102.8 MB (92.19%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int,int>> prevTemp;
         int n = temperatures.size();
         vector<int> res(n, 0);
 
-        for (int i = 0; i < n; i ++) {
-            int temp = temperatures[i];
-            while (!prevTemp.empty() && prevTemp.top().second < temp) {
-                res[prevTemp.top().first] = i - prevTemp.top().first;
-                prevTemp.pop();
+        for (int i = n - 2; i >= 0; i --) {
+            int j = i + 1;
+            // cout << "start " << i << endl;
+            while (j < n && temperatures[j] <= temperatures[i]) { // keep looking for larger
+                if (res[j] == 0) {
+                    j = n;
+                    break;
+                }
+
+                // cout << j << "add " << res[j] << endl;
+                j += res[j];
             }
-            prevTemp.push({i, temp});
+
+            if (j < n) {
+                // cout << "set" << endl;
+                res[i] = j - i;
+            }
         }
 
         return res;
