9f28766a491b84881bb8cea064024587042d64c1
Tony Chou <tony.chou@intel.com>
2026-02-01 04:51:30 -0800

    Time: 0 ms (100%), Space: 31.4 MB (7.5%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int l = 0;
         int r = n;
-        
-        while(l < r) {
-            int mid = l + (r - l) / 2;
-            int cur = nums[mid];
-            if(cur == target) {
+        int l = 0;
+
+        while (l < r) {
+            // cout << l << " " << r << endl;
+            int mid = l +  (r - l) / 2;
+            int num = nums[mid];
+            
+            if (num == target) {
                 return mid;
             }
-            else if(cur < target) {
-                l = mid + 1;
-            }
-            else {
+            else if (num > target) {
                 r = mid;
             }
+            else {
+                l = mid + 1;
+            }
         }
-        
+
         return -1;
     }
 };
