1a78bcc39ee1546c9daa24f5fd0eb0dae3afe62e
Tony Chou <tony.chou@intel.com>
2026-02-01 16:56:43 -0800

    Time: 0 ms (100%), Space: 15.2 MB (66.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
         int l = 0;
-        int r = n;
-        
-        while(l < r) {
+        int r = nums.size();
+
+        while (l < r) {
             int mid = l + (r - l) / 2;
-            bool curLarge = nums[mid] >= nums[l];
-            bool targetLarge = target >= nums[l];
-            
-            if(nums[mid] == target) {
+            int num = nums[mid];
+
+            if (num == target) {
                 return mid;
             }
-            
-            if(curLarge == targetLarge) {
-                if(target >= nums[mid]) {
+
+            bool curLeftPortion = num < nums[l];
+            bool targetLeftPortion = target < nums[l];
+
+            if (num < target) {
+                // same portion: go right
+                // different portion: go left
+                if (curLeftPortion == targetLeftPortion) {
                     l = mid + 1;
                 }
                 else {
                     r = mid;
                 }
             }
-            else {
-                if(target >= nums[mid]) {
+            else { // num > target
+                // same: go left
+                // different: got right
+                if (curLeftPortion == targetLeftPortion) {
                     r = mid;
                 }
                 else {
@@ -31,7 +36,36 @@ public:
                 }
             }
         }
-        
+
         return -1;
     }
 };
+
+// 4,5,6,7,0,1,2
+// l.          r
+//       m
+
+// num = 7
+// target = 0;
+
+// num > target
+
+// 0,1,2,4,5,6,7
+
+
+// 2,4,5,6,7.0,1
+// target = 7
+
+// l.          r
+
+// m = 6
+// mid < target
+
+// 5 6 7 0 1 2 3
+// l.          r
+//       m
+
+// target = 7
+// mid = 0
+
+// mid < target
