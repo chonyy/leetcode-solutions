5b5f3d053ad02e90fc0118561bfacc2b031c5471
Tony Chou <tony.chou@intel.com>
2026-02-01 19:28:39 -0800

    Time: 0 ms (100%), Space: 95.2 MB (44.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,52 +1,69 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len = nums1.size() + nums2.size();
-        int target = len / 2;
-        bool odd = len % 2;
-        
-        if(nums1.size() > nums2.size()) {
+        // otherwise remainLen can go negative
+        if (nums1.size() > nums2.size())
             swap(nums1, nums2);
-        }
-        
-        int n = nums1.size();
+
+        int m = nums1.size();
+        int n = nums2.size();
+        int len = m + n;
+        int target = len / 2;
+
         int l = 0;
-        int r = n;
-        
-        while(true) {
-            int mid1 = l + (r-l) / 2;
-            int mid2 = target - mid1;
-            
-            int l1 = mid1-1 >= 0 ? nums1[mid1-1] : INT_MIN;
-            int r1 = mid1 < nums1.size() ? nums1[mid1] : INT_MAX;
-            int l2 = mid2-1 >= 0 ? nums2[mid2-1] : INT_MIN;
-            int r2 = mid2 < nums2.size() ? nums2[mid2] : INT_MAX;
-            
-            cout << l1 << " " << r1 << endl;
-            cout << l2 << " " << r2 << endl;
-            
-            if(l1 <= r2 && l2 <= r1) {
-                if(odd) {
+        int r = m;
+        int idx1 = -1;
+        int idx2 = -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int remainLen = target - mid;
+
+            int l1 = mid - 1 >= 0 ? nums1[mid-1] : INT_MIN;
+            int r1 = mid < m ? nums1[mid] : INT_MAX;
+            int l2 = remainLen - 1 >= 0 ? nums2[remainLen-1] : INT_MIN;
+            int r2 = remainLen < n ? nums2[remainLen] : INT_MAX;
+
+            // cout << l1 << " " << r1 << endl;
+            // cout << l2 << " " << r2 << endl;
+
+            // TODO: check for out of bound
+            if (l1 <= r2 && l2 <= r1) {
+                if (len % 2) {
                     return min(r1, r2);
                 }
                 else {
                     return (double)(max(l1, l2) + min(r1, r2)) / 2;
                 }
             }
-            else if(l1 > r2) {
-                r = mid1;
+
+            if (l1 > r2) {
+                // cout << "take left" << endl;
+                r = mid;
             }
             else {
-                l = mid1 + 1;
+                // cout << "take right" << endl;
+                l = mid + 1;
             }
         }
-        
+
         return -1;
     }
 };
 
 
-// 1 3 5
-// 2 4 6
-    
-// 1 2 3 4 5 6
+// 1 2 3
+//     m
+
+// 4 5 
+// r
+
+// len = 5
+// target = 2
+
+
+// 1 2
+//   i
+
+// 3 4
+//   j
