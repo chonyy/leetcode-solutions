d1b30ab7752223c9ad042cb4eb9099cb2c36f4c3
Tony Chou <tony.chou@intel.com>
2026-02-04 02:47:18 -0800

    Time: 0 ms (100%), Space: 22.9 MB (54.58%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,44 +10,59 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr, *next = nullptr;
-        ListNode* cur = head;
-        
-        while(cur) {
-            next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-        }
-        
-        return prev;
-    }
-    
-    void merge(ListNode* l1, ListNode* l2) {
-        while(l2) {
-            ListNode* next = l1->next;
-            l1->next = l2;
-            l1 = l2;
-            l2 = next; 
-        }
-            
-    }
-    
     void reorderList(ListNode* head) {
-        ListNode* fast = head;
         ListNode* slow = head;
-        
-        while(fast->next && fast->next->next) {
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
-        
-        auto mid = slow->next;
+
+        ListNode* mid = slow->next;
         slow->next = nullptr;
         
-        auto revHead = reverse(mid);
-        
+        ListNode* revHead = reverse(mid);
+
         merge(head, revHead);
+    }
+
+    void merge(ListNode* head, ListNode* revHead) {
+        ListNode* sent = new ListNode();
+
+        while (head && revHead) {
+            // cout << head->val << " " << revHead->val << endl;
+            ListNode* nextHead = head->next;
+            ListNode* nextRevHead = revHead->next;
+
+            sent->next = head;
+            sent->next->next = revHead;
+
+            head = nextHead;
+            revHead = nextRevHead;
+            sent = sent->next->next;
+        }
+
+        if (head) {
+            sent->next = head;
+        }
+
+        if (revHead) {
+            sent->next = revHead;
+        }
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+
+        while (head) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
     }
 };
