cb4e561e999fe8e3ee0711ebc0112c8d59da0540
Tony Chou <tony.chou@intel.com>
2026-02-09 18:33:36 -0800

    Time: 0 ms (100%), Space: 27.9 MB (74.89%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,22 +11,28 @@
  */
 class Solution {
 public:
-    int ans = INT_MIN;
+    int res = INT_MIN;
     int maxPathSum(TreeNode* root) {
-        solve(root);
-        return ans;
+        findMax(root);
+        return res;
     }
-    
-    int solve(TreeNode* root) {
-        if(!root)
+
+    int findMax(TreeNode* root) {
+        if (!root) {
             return 0;
-        
-        int left = solve(root->left);
-        int right = solve(root->right);
-        left = max(left, 0);
-        right = max(right, 0);
-        
-        ans = max(ans, root->val + left + right);
-        return root->val + (right > left ? right : left);
+        }
+
+        int left = findMax(root->left);
+        int right = findMax(root->right);
+
+        int takeOne = max(left, right) + root->val;
+        int takeNone = root->val;
+        int takeBoth = left + right + root->val;
+
+        int choice = max(takeOne, takeNone);
+        choice = max(choice, takeBoth);
+        res = max(res, choice);
+
+        return max(takeNone, takeOne);
     }
 };
