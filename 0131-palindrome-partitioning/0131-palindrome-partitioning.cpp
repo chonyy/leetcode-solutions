da8c01e71b2e6db597d378d8c94dfc2bfea5b6f5
Tony Chou <tony.chou@intel.com>
2026-02-12 19:55:04 -0800

    Time: 31 ms (86.64%), Space: 53.7 MB (69.6%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
-    bool isPalindrome(string& s, int start, int end) {
-        // cout << s.substr(start, end-start+1) << endl;
-        while(start <= end) {
-            if(s[start] != s[end])
-                return false;
-            start ++;
-            end --;
-        }
-        return true;
+    unordered_set<string> mem;
+
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        vector<vector<string>> res;
+
+        backtrack(res, temp, s, 0);
+        
+        return res;
     }
-    
-    void solve(vector<vector<string>>& res, vector<string>& temp, string& s, int idx) {
-        if(idx == s.size()) {
+
+    void backtrack(vector<vector<string>>& res, vector<string>& temp, string& s, int idx) {
+        if (idx == s.size()) {
             res.push_back(temp);
             return;
         }
-        
-        for(int i = idx; i < s.size(); i ++) {
-            if(isPalindrome(s, idx, i)) {
-                string sub = s.substr(idx, i-idx+1);
+
+        int remainLen = s.size() - idx; 
+        for (int i = 1; i <= remainLen; i ++) {
+            int end = idx + i - 1;
+            
+            string sub = s.substr(idx, i);
+            if (mem.contains(sub) || isPalindrome(s, idx, end)) {
+                mem.insert(sub);
                 // cout << sub << endl;
                 temp.push_back(sub);
-                solve(res, temp, s, i+1);
+                backtrack(res, temp, s, idx + i);
                 temp.pop_back();
             }
         }
     }
-    
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> temp;
-        
-        solve(res, temp, s, 0);
-        
-        return res;
+
+    bool isPalindrome(string& s, int start, int end) {
+        while (start <= end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+
+            start ++;
+            end --;
+        }
+
+        return true;
     }
 };
