621987a2cfd9303a61c149ac391d600c89c34062
Tony Chou <tony.chou@intel.com>
2026-02-19 22:27:59 -0800

    Time: 103 ms (10.73%), Space: 41.4 MB (97.19%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,vector<pair<int,int>>> graph;
-        
-        for (auto& t : times) {
-            graph[t[0]].push_back({t[1], t[2]});
+        vector<int> nodeTime(n+1, INT_MAX);
+        nodeTime[k] = 0;
+
+        for (int i = 0; i < n; i ++) {
+            vector<int> temp = nodeTime;
+            for (auto& t : times) {
+                int cur = t[0];
+                int next = t[1];
+                int cost = t[2];
+                
+                if (nodeTime[cur] == INT_MAX) {
+                    continue;
+                }
+
+                if (nodeTime[cur] + cost < temp[next]) {
+                    // cout << next << " " << nodeTime[cur] + cost << endl;
+                    temp[next] = nodeTime[cur] + cost;
+                }
+            }
+
+            swap(temp, nodeTime);
         }
 
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0, k});
-
-        unordered_set<int> visited;
-
-        while (!pq.empty()) {
-            int curTime = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            // Check visited, can't be shorter after this
-            if (visited.contains(node)) {
+        int res = 0;
+        for (int i = 1; i <= n; i ++) {
+            if (i == k) {
                 continue;
             }
-            visited.insert(node);
-
-            // Return immediately after all nodes visited
-            if (visited.size() == n) {
-                return curTime;
-            }
-
-            auto& neighbors = graph[node];
-            for (auto& nei : neighbors) {
-                int time = nei.second;
-                int dest = nei.first;
-
-                pq.push({curTime+time, dest});
-            }
+            res = max(res, nodeTime[i]);
         }
 
-        return -1;
+        return res == INT_MAX ? -1 : res;
     }
 };
