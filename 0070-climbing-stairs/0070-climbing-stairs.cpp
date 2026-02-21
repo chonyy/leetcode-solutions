ee419523bd6fe6dc06abde927738bcaee38fc173
Tony Chou <tony.chou@intel.com>
2026-02-21 01:00:55 -0800

    Time: 0 ms (100%), Space: 7.9 MB (54.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1)
+        if (n == 1) {
             return 1;
-        
-        if(n == 2)
+        }
+
+        if (n == 2) {
             return 2;
-        
-        int prevTwo = 1;
+        }
+
         int prevOne = 2;
-        
-        for(int i = 3; i <= n; i ++) {
+        int prevTwo = 1;
+
+        for (int i = 0; i < n-2; i ++) {
             int temp = prevOne + prevTwo;
             prevTwo = prevOne;
             prevOne = temp;
         }
-        
+
         return prevOne;
     }
 };
