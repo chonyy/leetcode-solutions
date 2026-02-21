da72eabdef4671344e7fce6ae524c02898ca50ca
Tony Chou <tony.chou@intel.com>
2026-02-21 01:17:41 -0800

    Time: 0 ms (100%), Space: 10 MB (89.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
-        int prevTwo = 0;
         int prevOne = 0;
-        
-        for(int i = 0; i < n; i ++) {
-            int temp = max(prevTwo + nums[i], prevOne);
+        int prevTwo = 0;
+
+        for (int i = 0; i < n; i ++) {
+            int temp = max(prevOne, prevTwo + nums[i]);
             prevTwo = prevOne;
             prevOne = temp;
         }
-        
+
         return prevOne;
     }
 };
+
+
+// 1, 2, 3, 1
+
+
+// rob = 0
+// notRob = 0
+
