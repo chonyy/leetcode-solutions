19f6bb6ff32556d4ea07ba96b7e6d5e33ee45641
Tony Chou <tony.chou@intel.com>
2026-02-21 01:07:53 -0800

    Time: 0 ms (100%), Space: 17.6 MB (73.04%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        
-        int prevTwo = 0;
+
+        if (n == 1) {
+            return cost[0];
+        }
+
+        if (n == 2) {
+            return min(cost[0], cost[1]);
+        }
+
         int prevOne = 0;
-        
-        for(int i = 2; i <= n; i ++) {
+        int prevTwo = 0;
+
+        for (int i = 2; i <= n; i ++) {
             int temp = min(prevOne + cost[i-1], prevTwo + cost[i-2]);
             prevTwo = prevOne;
             prevOne = temp;
         }
-        
+
         return prevOne;
     }
 };
