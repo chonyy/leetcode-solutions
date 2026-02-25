407d39dbcf6fe09b95b86bb8a87c92e2895f0c5c
Tony Chou <tony.chou@intel.com>
2026-02-24 19:08:13 -0800

    Time: 0 ms (100%), Space: 20.5 MB (33.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,65 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jumps = 0;
         int curReach = 0;
         int maxReach = 0;
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i ++) {
+        int step = 0;
+
+        if (nums.size() == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < nums.size(); i ++) {
+            // update max reach
             maxReach = max(maxReach, i + nums[i]);
-            
-            if(i == curReach && i != n-1) {
-                jumps ++;
+
+            // add step if at curReach
+            if (i == curReach) {
+                step ++;
                 curReach = maxReach;
-                maxReach = 0;
             }
-        }        
-        
-        return jumps;
+
+            // exit early if reach last idx
+            if (curReach >= nums.size()-1) {
+                return step;
+            }
+        }
+
+        return step;
     }
 };
+
+
+// 2,3,1,1,4
+//     i
+
+// curReach = 0
+// maxReach = 0
+// step = 0
+
+
+// curReach = 2
+// maxReach = 2
+// step = 1
+
+// curReach = 2
+// maxReach = 4
+// step = 1
+
+// curReach = 4
+// maxReach = 4
+// step = 2
+
+
+
+// update max Reach
+
+// if idx == curReach
+//     step ++
+//     curReach = maxReach
+
+
+// 0 1 1
+//     2 2 2
+//       2
+//         3
