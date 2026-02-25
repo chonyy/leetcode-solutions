3cf8c7567c9c7b3ff07b7f30ef4f4e47d552bc06
Tony Chou <tony.chou@intel.com>
2026-02-24 18:50:47 -0800

    Time: 3 ms (28.16%), Space: 52.4 MB (49.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int reach = 0;
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i ++) {
-            if(i > reach)
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.size(); i ++) {
+            if (i > maxReach) {
                 return false;
-            reach = max(reach, i + nums[i]);
+            }
+
+            int num = nums[i];
+            maxReach = max(maxReach, i + num);
+
+            if (maxReach >= nums.size() - 1) {
+                return true;
+            }
         }
-        
+
         return true;
     }
 };
+
+// 2,3,1,1,4
+//   i
+
+// keep track of maxReachIdx
+
+// maxReach = 2
+
+// maxReach = 1 + 3
+
+// if maxReach >= num.size()-1
+//     return true
+
+// if get to anypoint that's larger than maxReach, return false
