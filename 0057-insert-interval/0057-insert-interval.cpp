fd8218bc4d78f9dc7ba9e7b089b3788e0a6f5b73
Tony Chou <tony.chou@intel.com>
2026-02-25 22:56:23 -0800

    Time: 0 ms (100%), Space: 22 MB (14.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,45 +1,87 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int i = 0;
-        int n = intervals.size();
         vector<vector<int>> res;
-        
-        if(n == 0) {
+        int n = intervals.size();
+        int idx = 0;
+
+        if (n == 0) {
             return {newInterval};
         }
 
-        while(i < n) {
-            auto& inter = intervals[i];
-            if(newInterval[0] <= inter[1])
-                break;
-            res.push_back(inter);
-            i ++;
+        // step1: merge beginning non-overlap
+        while (idx < n && intervals[idx][1] < newInterval[0]) {
+            res.push_back(intervals[idx]);
+            idx ++;
         }
-        
-        while(i < n) {
-            auto& inter = intervals[i];
-            if(newInterval[1] < inter[0]) {
-                break;
-            }
-            
-            newInterval[0] = min(newInterval[0], inter[0]);
-            newInterval[1] = max(newInterval[1], inter[1]);
-                
-            i ++;
+
+        int intervalStart = INT_MAX;
+        int intervalEnd = -1;
+
+        // step2: merge overlaps
+        while (idx < n && intervals[idx][0] <= newInterval[1]) {
+            intervalStart = min(intervalStart, intervals[idx][0]);
+            intervalStart = min(intervalStart, newInterval[0]);
+
+            intervalEnd = max(intervalEnd, intervals[idx][1]);
+            intervalEnd = max(intervalEnd, newInterval[1]);
+            idx ++;
         }
-        
-        res.push_back({newInterval});
-        
-        while(i < n) {
-            res.push_back(intervals[i]);
-            i ++;
+        if (intervalStart != INT_MAX) {
+            res.push_back({intervalStart, intervalEnd});
         }
-        
+        else {
+            res.push_back(newInterval);
+        }
+
+        // step3: merge final non-overlap
+        while (idx < n) {
+            res.push_back(intervals[idx]);
+            idx ++;
+        }
+
         return res;
     }
 };
 
-// push all nonoverlapping
-// keep track of min and max of overlapping
-// push remain
+// [1,2],[3,5],[6,7],[8,10],[12,16]
+//                            i
+
+// [4,8]
+
+// intervalStart = 3
+// intervalEnd = 10
+
+// res: [1,2][3,10][12,16]
+
+
+// [1,3],[6,9]
+//  x
+
+// [2,5]
+
+// [1,5]
+
+// loop through intervals to find start
+//     start = inter1.end >= inter2.start
+
+// start merging
+//     skip until inter1.start > inter2.end or the end
+//     insert [originalStart, max(inter1.end, inter2.end)]
+
+
+
+// [1,2],[3,5],[6,7],[8,10],[12,16]
+//                     i
+
+// [4,8]
+
+
+// [1,2][3, 10][12,16]
+
+//      1      5
+//   0  
+
+
+//       o     o
+// n  n   
