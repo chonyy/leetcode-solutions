b4f95461b533b7b84662607d611119ba0658edcc
Tony Chou <tony.chou@intel.com>
2026-03-01 18:33:17 -0800

    Time: 0 ms (100%), Space: 28 MB (86.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
+    // voting algorithm, only when majority guarantee exists
     int majorityElement(vector<int>& nums) {
-        int cur = nums[0];
-        int count = 0;
-        
-        for(int num : nums) {
-            if(cur == num) {
+        int res = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i ++) {
+            if (nums[i] == res) {
                 count ++;
             }
             else {
                 count --;
-            }
-            
-            if(count < 0) {
-                count = 1;
-                cur = num;
+
+                if (count < 0) {
+                    count = 1;
+                    res = nums[i];
+                }
             }
         }
-        
-        return cur;
+
+        return res;
     }
 };
