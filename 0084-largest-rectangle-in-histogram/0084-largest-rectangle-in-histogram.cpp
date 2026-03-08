95dec3b3e8dae33c7c0a02e488894bc0e93a47f1
Tony Chou <tony.chou@intel.com>
2026-03-07 21:10:49 -0800

    Time: 39 ms (14.02%), Space: 89.3 MB (11.25%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<int> stk;
         int n = heights.size();
-        stack<pair<int,int>> stk; // startIdx, height
-        int res = 0;
 
+        vector<int> shorterRight(n);
+        // from left to right, get distance to next shorter
         for (int i = 0; i < n; i ++) {
-            int h = heights[i];
-            int popCount = 0;
-            int start = i;
-            // pop all elemetns larger
-            // keep trace of pop count
-            while(!stk.empty() && stk.top().second > h) {
-                int startIdx = stk.top().first;
-                int prevHeight = stk.top().second;
-                int width = i - startIdx;
-                int rec = width * prevHeight;
-
-                res = max(res, rec);
-                start = startIdx;
+            while (!stk.empty() && heights[stk.top()] > heights[i]) {
+                // cout << stk.top() << " shorter right " << i - stk.top() << endl;;
+                shorterRight[stk.top()] = i - stk.top();
                 stk.pop();
             }
-
-            // use pop count of new startIdx
-            stk.push({start, h});
+            stk.push(i);
         }
 
         while (!stk.empty()) {
-            int startIdx = stk.top().first;
-            int prevHeight = stk.top().second;
-            int width = n - startIdx;
-            int rec = width * prevHeight;
-
-            res = max(res, rec);
+            shorterRight[stk.top()] = n - stk.top();
             stk.pop();
         }
-        
+
+        vector<int> shorterLeft(n);
+        // from right to left, get distance to next shorter
+        for (int i = n -1; i >= 0; i --) {
+            while (!stk.empty() && heights[stk.top()] > heights[i]) {
+                shorterLeft[stk.top()] = stk.top() - i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+
+        while (!stk.empty()) {
+            shorterLeft[stk.top()] = stk.top() - (-1);
+            stk.pop();
+        }
+
+        // res = max(height * (shortLeft + shortRight - 1))
+        int res = 0;
+
+        for (int i = 0; i < n; i ++) {
+            int width = shorterLeft[i] + shorterRight[i] - 1;
+            // cout << heights[i] << " " << width << " " << shorterLeft[i] << " " << shorterRight[i] << " " << endl;
+            res = max(res, width * heights[i]);
+        }
+
         return res;
     }
 };
