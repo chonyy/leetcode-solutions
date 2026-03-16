7daa7f3bf88804922a3577c93b551b9bd978463b
Tony Chou <tony.chou@intel.com>
2026-03-16 00:57:51 -0700

    Time: 2 ms (8.55%), Space: 9.4 MB (74.79%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> res(n, vector<int>(n));
-        
+        vector<vector<int>> res(n, vector<int>(n, 0));
+
         int rowStart = 0;
-        int rowEnd = n - 1;
+        int rowEnd = n-1;
         int colStart = 0;
-        int colEnd = n - 1;
-        int num = 1;
-        
-        while(rowStart <= rowEnd && colStart <= colEnd) {
-            for(int i = colStart; i <= colEnd; i ++) {
-                res[rowStart][i] = num ++;
+        int colEnd = n -1;
+
+        int cur = 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i ++) {
+                res[rowStart][i] = cur;
+                cur ++;
             }
             rowStart ++;
-            
-            for(int i = rowStart; i <= rowEnd;i ++) {
-                res[i][colEnd] = num ++;
+
+            for (int i = rowStart; i <= rowEnd; i ++) {
+                res[i][colEnd] = cur;
+                cur ++;
             }
             colEnd --;
-            
-            for(int i = colEnd; i >= colStart; i --) {
-                if(rowStart <= rowEnd)
-                    res[rowEnd][i] = num ++;
+
+            for (int i = colEnd; i >= colStart; i --) {
+                res[rowEnd][i] = cur;
+                cur ++;
             }
             rowEnd --;
-            
-            for(int i = rowEnd; i >= rowStart; i --) {
-                if(colStart <= colEnd)
-                    res[i][colStart] = num ++;
+
+            for (int i = rowEnd; i >= rowStart; i --) {
+                res[i][colStart] = cur;
+                cur ++;
             }
             colStart ++;
         }
-        
+
         return res;
     }
 };
