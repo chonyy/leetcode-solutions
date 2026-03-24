0c5df80fd0b34b8c2d58acf4acf5797a90a10c3d
Tony Chou <tony.chou@intel.com>
2026-03-24 01:49:59 -0700

    Time: 7 ms (2.88%), Space: 11.6 MB (96.06%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,64 +1,40 @@
 class Solution {
 public:
-    unordered_map<string, vector<pair<string,double>>> graph;
-
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        buildGraph(equations, values);
-
-
-        vector<double> res;
-        for (auto& q : queries) {
-            res.push_back(find(graph, q[0], q[1]));
-        }
-
-        return res;
-    }
-
-    void buildGraph(vector<vector<string>>& equations, vector<double>& values) {
-        int n = equations.size();
+        unordered_map<string, unordered_map<string, double>> graph;
+        int n = values.size();
 
         for (int i = 0; i < n; i ++) {
             auto& e = equations[i];
             double val = values[i];
-            graph[e[0]].push_back({e[1], val});
-            graph[e[1]].push_back({e[0], 1 / val});
-        }
-    }
-
-    double find(unordered_map<string, vector<pair<string,double>>> graph, string& src, string& dest) {
-        if (src == dest) {
-            return graph.contains(src) ? 1.0 : -1.0;
+            graph[e[0]][e[1]] = val;
+            graph[e[1]][e[0]] = 1 / val;
         }
 
-        unordered_set<string> visited;
-        queue<pair<string,double>> q;
-
-        q.push({src, 1.0});
-        
-        while(!q.empty()) {
-            string curNode = q.front().first;
-            double curValue = q.front().second;
-            q.pop();
-
-            visited.insert(curNode);
-
-            auto& neighbors = graph[curNode];
-            for(pair<string, double> nei : neighbors) {
-                string& node = nei.first;
-                double value = nei.second;
-
-                if (visited.contains(node)) {
-                    continue;
+        for (auto& e1 : graph) {
+            string k = e1.first;
+            for (auto& e2 : graph) {
+                string i = e2.first;
+                for (auto& e3 : graph) {
+                    string j = e3.first;
+                    // set i => j, if i => k and k => j is set
+                    if (graph[i].contains(k) && graph[k].contains(j)) {
+                        graph[i][j] = graph[i][k] * graph[k][j];
+                    }
                 }
-
-                if (node == dest) {
-                    return value * curValue; 
-                }
-
-                q.push({node, value * curValue});
             }
         }
 
-        return -1.0;
-    } 
+        vector<double> res;
+
+        for (auto& q : queries) {
+            if (!graph.contains(q[0]) || !graph.contains(q[1])) {
+                res.push_back(-1.0);
+                continue;
+            }
+            res.push_back(graph[q[0]][q[1]] == 0 ? -1.0 : graph[q[0]][q[1]]);
+        }
+
+        return res;
+    }
 };
