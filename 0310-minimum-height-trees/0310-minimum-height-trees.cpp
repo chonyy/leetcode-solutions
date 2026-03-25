603fb79be4836ebf837277c678fa5e176c6795d5
Tony Chou <tony.chou@intel.com>
2026-03-24 17:37:32 -0700

    Time: 83 ms (14.43%), Space: 70.3 MB (12.17%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         unordered_map<int,int> inDegree;
-        unordered_map<int, unordered_set<int>> graph;
+        unordered_map<int, vector<int>> graph;
 
         if (edges.size() == 0) {
             return {0};
@@ -11,8 +11,8 @@ public:
         for (auto& e : edges) {
             inDegree[e[0]] ++;
             inDegree[e[1]] ++;
-            graph[e[0]].insert(e[1]);
-            graph[e[1]].insert(e[0]);
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
         }
 
         queue<int> q;
