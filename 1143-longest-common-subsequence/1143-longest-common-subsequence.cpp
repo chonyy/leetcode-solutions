f8ab9c9228ec70c0fd324b539817c2156909523c
Tony Chou <tony.chou@intel.com>
2026-03-30 00:42:51 -0700

    Time: 28 ms (43.46%), Space: 27.5 MB (29.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,12 +3,12 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
-        
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
-        
-        for(int i = 1; i <= m ; i ++) {
-            for(int j = 1; j <= n; j ++) {
-                if(text1[i-1] == text2[j-1]) {
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        for (int i = 1; i <= m; i ++) {
+            for (int j = 1; j <= n; j ++) {
+                if (text1[i-1] == text2[j-1]) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
                 else {
@@ -17,6 +17,22 @@ public:
             }
         }
         
-        return dp[m][n];
+        return dp[m][n]; // can shrink to 1d
     }
 };
+
+// dp[i][j] = longest common subsequence between text1[0:i-1] and text2[0:j-1] 
+
+// if text1[i] == text2[j], dp[i+1][j+1] = dp[i][j]] + 1
+
+// else dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+
+// abcde
+// i
+
+// ace
+//  j
+
+
+// ab
+// ac
