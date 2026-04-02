0aed79905289569caa17df4b1a059972c3daa32c
Tony Chou <tony.chou@intel.com>
2026-04-02 05:24:26 -0700

    Time: 4 ms (92.8%), Space: 10.5 MB (77.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<uint64_t> dp(amount+1);
+        vector<unsigned long long> dp(amount+1, 0);
         dp[0] = 1;
-        
+
         for (int coin : coins) {
             for (int i = 1; i <= amount; i ++) {
-                if (coin > i) {
+                int need = i - coin;
+
+                if (need < 0) {
                     continue;
                 }
 
-                int need = i - coin;
                 dp[i] += dp[need];
             }
         }
