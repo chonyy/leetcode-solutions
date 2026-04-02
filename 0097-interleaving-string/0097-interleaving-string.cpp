7d2a395beae45e8dd508d44112f453c56ae82d47
Tony Chou <tony.chou@intel.com>
2026-04-02 05:40:35 -0700

    Time: 2 ms (63.26%), Space: 9.3 MB (51.84%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,59 +1,49 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0] = true;
+        int m = s1.size();
+        int n = s2.size();
 
-        if (s1.size() + s2.size() != s3.size()) {
+        if (m + n != s3.size()) {
             return false;
         }
 
-        for (int i = 0; i <= n; i ++) {
-            for (int j = 0; j <= m; j ++) {
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1));
+
+        for (int i = 0; i <= m; i ++) {
+            for (int j = 0; j <= n; j ++) {
                 if (i == 0 && j == 0) {
+                    dp[i][j] = true;
                     continue;
                 }
 
                 int k = i + j - 1;
-                if (i == 0) {
-                    dp[i][j] = dp[i][j-1] && (s2[j-1] == s3[k]);;
-                }
-                else if (j == 0) {
-                    dp[i][j] = dp[i-1][j] && (s1[i-1] == s3[k]);
-                }
-                else {
-                    bool fromS2 = dp[i][j-1] && (s2[j-1] == s3[k]);
-                    bool fromS1 = dp[i-1][j] && (s1[i-1] == s3[k]);
-                    dp[i][j] = fromS2 || fromS1;
-                }
+                bool fromS1 = i > 0 && dp[i-1][j] && s1[i-1] == s3[k];
+                bool fromS2 = j > 0 && dp[i][j-1] && s2[j-1] == s3[k];
+                dp[i][j] = fromS1 || fromS2;
             }
         }
-
-        return dp[n][m];
+        
+        return dp[m][n];
     }
 };
 
-// two pointers will not work
 
-// ab
+// aabcc
+//   i
+
+// dbbca
+//  j
+
+// aadbbcbcac
+//    k
+
+
+// bbacc
+//    i
+
+// ae
 // i
 
-// acc
-// j
-
-// acacb
-// k
-
-
-
-//     "" a b
-// ""  0  t f
-// a.  t  f  
-// c
-// c
- 
-
-// acacb
+// bbaeacc
 //   k
