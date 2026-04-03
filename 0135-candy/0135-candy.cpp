a906c7802320fa21c84c8bf03daeb2cc11f98bf3
Tony Chou <tony.chou@intel.com>
2026-04-03 16:41:16 -0700

    Time: 0 ms (100%), Space: 22.8 MB (77.71%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candy(n, 1);
+
+        for (int i = 1; i < n; i ++) {
+            if (ratings[i] > ratings[i-1]) {
+                candy[i] = candy[i-1] + 1;
+            }
+        }
+
+        for (int i = n-2; i >= 0; i --) {
+            if (ratings[i] > ratings[i+1]) {
+                candy[i] = max(candy[i], candy[i+1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int c : candy) {
+            sum += c;
+        }
+
+        return sum;
+    }
+};
