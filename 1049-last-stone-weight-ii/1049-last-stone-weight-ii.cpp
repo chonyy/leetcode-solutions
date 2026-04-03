e77081e8ec44e694ece9a596210f9a4c220f8c91
Tony Chou <tony.chou@intel.com>
2026-04-02 17:05:31 -0700

    Time: 0 ms (100%), Space: 10.5 MB (91.51%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+
+        for (int stone : stones) {
+            sum += stone;
+        }
+
+        int target = sum / 2;
+
+        vector<bool> dp(target+1);
+        dp[0] = true;
+
+        // split stones to two groups, minimize the diff between two groups
+        // unbounded knapsack to find the target
+        for (int stone : stones) {
+            for (int i = target; i > 0; i --) {
+                int need = i - stone;
+                if (need < 0) {
+                    break;
+                }
+
+                if (dp[need]) {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        int closest = 0;
+        for (int i = target; i >= 0; i --) {
+            if (dp[i]) {
+                closest = i;
+                break;
+            }
+        }
+
+        return (sum - closest) - closest;
+    }
+};
+
+// 2,7,4,1,8,1
+
+// 8 7 4 2 1 1 
+
+// 1 4 2 1 1
+
+// 2 1 1 1
+// 1 1 1
