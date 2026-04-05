9c2281faa28cd05f2406362b1ce0e1333d4d7879
Tony Chou <tony.chou@intel.com>
2026-04-05 16:44:13 -0700

    Time: 48 ms (55.39%), Space: 28.9 MB (91.89%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,34 +6,36 @@ public:
         vector<vector<int>> res;
 
         for (int i = 0; i < n - 2; i ++) {
-            int l = i + 1;
-            int r = n - 1;
-            int target = -nums[i];
-
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
 
+            int target = -nums[i];
+            int l = i + 1;
+            int r = n - 1;
             while (l < r) {
-                int cur = nums[l]+ nums[r];
+                int sum = nums[l] + nums[r];
 
-                if (cur == target) {
+                if (sum == target) {
                     res.push_back({nums[i], nums[l], nums[r]});
 
-                    while (l < n -1 && nums[l] == nums[l+1]) {
+                    while (l < r && nums[l] == nums[l+1]) {
                         l ++;
                     }
-
-                    while (r > 0 && nums[r] == nums[r-1]) {
+                    while (l < r && nums[r] == nums[r-1]) {
                         r --;
                     }
+
+                    l ++;
+                    r --;
                 }
 
-                if (cur < target) {
-                    l ++;
-                }
-                else {
+                if (sum > target) {
                     r --;
+                }
+
+                if (sum < target) {
+                    l ++;
                 }
             }
         }
