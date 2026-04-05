abbcb7e5c65945a11d01d2e1c8f75df5af570471
Tony Chou <tony.chou@intel.com>
2026-04-05 15:31:35 -0700

    Time: 45 ms (98.19%), Space: 173.2 MB (68.92%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,118 +1,65 @@
-class DoublyNode {
-public:
-    int key;
-    int val;
-    DoublyNode* prev;
-    DoublyNode* next;
-
-    DoublyNode(int key, int value) {
-        this->key = key;
-        val = value;
-    }
-};
-
-class LinkedList {
-public:
-    // left: new, right: old
-    DoublyNode* left;
-    DoublyNode* right;
-    int cap;
-    int size;
-
-    LinkedList(int cap) {
-        this->cap = cap;
-        size = 0;
-        left = new DoublyNode(0, 0);
-        right = new DoublyNode(0, 0);
-        left->next = right;
-        right->prev = left;
-    }
-
-    DoublyNode* getRight() {
-        return right->prev;
-    }
-
-    void remove(DoublyNode* node) {
-        DoublyNode* prev = node->prev;
-        DoublyNode* next = node->next;
-
-        prev->next = next;
-        next->prev = prev;
-
-        size --;
-    }
-
-    void addToLeft(DoublyNode* node) {
-        DoublyNode* next = left->next;
-
-        left->next = node;
-        node->prev = left;
-
-        node->next = next;
-        next->prev = node;
-
-        size ++;
-    }
-
-    bool isFull() {
-        return size == cap;
-    }
-};
-
 class LRUCache {
     // map: key -> DoublyNode
     // doublyLinkedList: ll
 private:
-    unordered_map<int, DoublyNode*> map;
-    LinkedList ll;
+    unordered_map<int, list<pair<int,int>> :: iterator> map;
+    list<pair<int,int>> ll;
+    int cap;
+    int size;
 public:
-    LRUCache(int capacity) : ll(capacity) {
+    LRUCache(int capacity) {
+        cap = capacity;
+        size = 0;
     }
     
     int get(int key) {
         // check if contain in map
         // return -1 if not
-        if (!map.contains(key)){
+        if (!map.contains(key)) {
             return -1;
         }
 
         // get DoublyNode from map
-        // remove from ll
-        // push to front
-        DoublyNode* node = map[key];
-        ll.remove(node);
-        ll.addToLeft(node);
+        // remove from ll, push to front
+        // reset iterator
+        auto it = map[key];
+        ll.splice(ll.begin(), ll, it);
+        map[key] = ll.begin();
 
-        return node->val;
+        return it->second;
     }
     
     void put(int key, int value) {
         // check if contain in map
         // if yes, remove form ll and push to front
+        // set the value
+        // reset iterator
         if (map.contains(key)) {
-            DoublyNode* node = map[key];
-            node->val = value;
-            ll.remove(node);
-            ll.addToLeft(node);
+            auto it = map[key];
+            it->second = value;
+            ll.splice(ll.begin(), ll, it);
+            map[key] = ll.begin();
             return;
         }
 
         // if full
         // get toDelete node from back
+        // remove toDelete key from map
         // remove toDelete from ll
-        // remove toDelete from map
-        if (ll.isFull()) {
-            DoublyNode* toDelete = ll.getRight();
-            ll.remove(toDelete);
-            map.erase(toDelete->key);
+        if (size == cap) {
+            auto toDelete = ll.back();
+            map.erase(toDelete.first);
+            ll.pop_back();
+            size --;
         }
-
 
         // create new DoublyNode and push to front
         // add entry in map
-        DoublyNode* newNode = new DoublyNode(key, value);
-        ll.addToLeft(newNode);
-        map[key] = newNode;
+        // increment size
+        pair<int,int> newNode = {key, value};
+        ll.push_front(newNode);
+        map[key] = ll.begin();
+        size ++;
     }
 };
 
