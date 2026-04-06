46225e37e975e89dda1a5bd8deed820bc605707d
Tony Chou <tony.chou@intel.com>
2026-04-05 17:44:18 -0700

    Time: 11 ms (65.3%), Space: 9.5 MB (65.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,48 +2,37 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n+1, 0));
         int maxLen = 1;
         int startIdx = 0;
 
-        for (int i = n; i >= 1; i --) {
-            for (int j = i; j <= n; j ++) {
-                if (i == j) {
-                    dp[i][j] = 1;
-                    continue;
-                }
+        for (int i = 0; i < n; i ++) {
+            int newLen = max(tryExpand(s, i, i), tryExpand(s, i, i+1));
 
-                if (s[i-1] == s[j-1] && (dp[i+1][j-1] || j - i <= 1)) {
-                    dp[i][j] = dp[i+1][j-1] + 2;
-                }
-                
-                if (dp[i][j] > maxLen) {
-                    maxLen = dp[i][j];
-                    startIdx = i -1;
-                }
+            if (newLen > maxLen) {
+                maxLen = newLen;
+                startIdx = i - (maxLen - 1) / 2;
             }
         }
 
         return s.substr(startIdx, maxLen);
     }
+
+    int tryExpand(string& s, int l, int r) {
+        int len = 0;
+        while (l >= 0 && r < s.size()) {
+            if (s[l] != s[r]) {
+                break;
+            }
+
+            len = r - l + 1;
+
+            l --;
+            r ++;
+        }
+
+        return len;
+    }
 };
 
-// dp[i][i] = 1;
-
-// dp[i][j] = s[i] == s[j] && (j - i <= 1 || dp[i+1][j-1]), +2
-
-
-//   b a b a d
-// b 1 0 3 0 0
-// a.  1 0 1 0
-// b.    1 0 0
-// a       1 0
-// d.        1
-
-
-//     c b b d
-//   0 0 0 0 0
-// c 0
-// b 0.  1 
-// b 0.    1 0
-// d 0       1
+// b a a b
+//   i
