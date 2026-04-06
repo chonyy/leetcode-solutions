edf027e7812230eb64c2e1b91e21faa6c4c8e59d
Tony Chou <tony.chou@intel.com>
2026-04-05 17:37:06 -0700

    Time: 268 ms (16.55%), Space: 292.7 MB (6.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,36 +2,48 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        int start = 0;
-        int maxLen = 0;
+        vector<vector<int>> dp(n + 1, vector<int>(n+1, 0));
+        int maxLen = 1;
+        int startIdx = 0;
 
-        for (int i = 0; i < n; i ++) {
-            tryExpand(s, i-1, i, start, maxLen);
-            tryExpand(s, i, i, start, maxLen);
+        for (int i = n; i >= 1; i --) {
+            for (int j = i; j <= n; j ++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                if (s[i-1] == s[j-1] && (dp[i+1][j-1] || j - i <= 1)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+                
+                if (dp[i][j] > maxLen) {
+                    maxLen = dp[i][j];
+                    startIdx = i -1;
+                }
+            }
         }
 
-        return s.substr(start, maxLen);
-    }
-
-    void tryExpand(string& s, int l, int r, int& start, int& maxLen) {
-        while (l >= 0 && r < s.size()) {
-            if (s[l] != s[r]) {
-                break;
-            }
-
-            int len = r - l + 1;
-
-            if (len > maxLen) {
-                maxLen = len;
-                start = l;
-            }
-
-            l --;
-            r ++;
-        }
+        return s.substr(startIdx, maxLen);
     }
 };
 
-// dp[0][0] = 1
-// dp[1][1] = 1
+// dp[i][i] = 1;
 
+// dp[i][j] = s[i] == s[j] && (j - i <= 1 || dp[i+1][j-1]), +2
+
+
+//   b a b a d
+// b 1 0 3 0 0
+// a.  1 0 1 0
+// b.    1 0 0
+// a       1 0
+// d.        1
+
+
+//     c b b d
+//   0 0 0 0 0
+// c 0
+// b 0.  1 
+// b 0.    1 0
+// d 0       1
