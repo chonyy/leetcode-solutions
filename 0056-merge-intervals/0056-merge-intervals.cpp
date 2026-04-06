27739b67767faa35d9f91ba9ea7beac396312b69
Tony Chou <tony.chou@intel.com>
2026-04-06 16:08:19 -0700

    Time: 3 ms (89.24%), Space: 23.9 MB (59.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> res;
 
-        int i = 0;
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        int i = 1;
+
         while (i < n) {
-            int j = i + 1;
-            while (j < n && intervals[j][0] <= intervals[i][1]) {
-                intervals[i][1] = max(intervals[i][1], intervals[j][1]);
-                j ++;
+            auto& prev = res.back();
+            
+            while (i < n && prev[1] >= intervals[i][0]) {
+                prev[1] = max(prev[1], intervals[i][1]);
+                i ++;
             }
 
-            res.push_back(intervals[i]);
-            i = j;
+            if (i < n) {
+                res.push_back(intervals[i]);
+            }
+            
+            i ++;
         }
 
         return res;
