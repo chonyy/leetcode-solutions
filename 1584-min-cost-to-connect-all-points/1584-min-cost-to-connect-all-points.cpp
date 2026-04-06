355fb602ea3a079e1ea3ed8d31f73296b41fe998
Tony Chou <tony.chou@intel.com>
2026-04-06 16:46:20 -0700

    Time: 839 ms (15.41%), Space: 180.7 MB (15.33%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,50 +1,72 @@
+class UnionFind {
+private:
+    unordered_map<int,int> parent;
+    unordered_map<int,int> rank;
+public:
+    UnionFind(int n) {
+        for (int i = 0; i < n; i ++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+
+        return parent[node] = find(parent[node]);
+    }
+
+    bool unionNode(int n1, int n2) {
+        int p1 = find(n1);
+        int p2 = find(n2);
+
+        if (p1 == p2) {
+            return false;
+        }
+
+        if (rank[p1] >= rank[p2]) {
+            parent[p2] = p1;
+            rank[p1] ++;
+        }
+        else {
+            parent[p1] = p2;
+            rank[p2] ++;
+        }
+
+        return true;
+    }
+};
+
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_set<int> visited;
-        unordered_map<int, vector<pair<int,int>>> graph; // pointIdx => {dist, pointIdx}
-
         int n = points.size();
+        UnionFind uf = UnionFind(n);
+
+        vector<vector<int>> edges;
 
         for (int i = 0; i < n; i ++) {
             for (int j = i + 1; j < n; j ++) {
                 auto& p1 = points[i];
                 auto& p2 = points[j];
+                int cost = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
 
-                int dist = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
-                // cout << dist << endl;
-                graph[i].push_back({dist, j});
-                graph[j].push_back({dist, i});
+                edges.push_back({cost, i, j});
             }
         }
 
-        int curCost = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        pq.push({0, 0});
+        sort(edges.begin(), edges.end());
+        
+        int res = 0;
 
-        while (visited.size() != n) {
-            int point = pq.top().second;
-            int cost = pq.top().first;
-            pq.pop();
-
-            if (visited.contains(point)) {
-                continue;
-            }
-            visited.insert(point);
-
-            // add cost
-            curCost += cost;
-
-            // traverse neighbors
-            // skip visited
-            vector<pair<int,int>> neighbors = graph[point];
-            // cout << neighbors.size() << endl;
-
-            for (auto& nei : neighbors) {
-                pq.push(nei);
+        for (auto& e : edges) {
+            if (uf.unionNode(e[1], e[2])) {
+                res += e[0];
             }
         }
 
-        return curCost;
+        return res;
     }
 };
