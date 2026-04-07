1275316349b3415c6cfd148692d89a1150725f43
Tony Chou <tony.chou@intel.com>
2026-04-07 15:48:58 -0700

    Time: 0 ms (100%), Space: 10.3 MB (94.75%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
         int n = s.size();
         vector<bool> dp(n+1, false);
         dp[0] = true;
 
         for (int i = 1; i <= n; i ++) {
-            for (auto& word : dict) {
-                if (word.size() > i) {
+            for (auto& w : wordDict) {
+                int len = w.size();
+                if (i - len < 0) {
                     continue;
                 }
 
-                int len = word.size();
-                string sub = s.substr(i - len, len);
-
-                if (sub == word && dp[i - len]) {
-                    dp[i] = true;
-                    break;
+                if (dp[i - len]) {
+                    string sub = s.substr(i - len, len);
+                    if (sub == w) {
+                        dp[i] = true;
+                        break;
+                    }
                 }
             }
         }
@@ -25,6 +25,3 @@ public:
         return dp[n];
     }
 };
-
-// dp[i] = possible to form words till len i
-// dp[i] = dp[i - len] && len substring is in word
