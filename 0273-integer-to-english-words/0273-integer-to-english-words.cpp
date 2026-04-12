d7459240771b8a900d1712cb85ffeba8895a4b36
Tony Chou <tony.chou@intel.com>
2026-04-12 15:00:37 -0700

    Time: 0 ms (100%), Space: 13.1 MB (5.04%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,45 +1,64 @@
 class Solution {
 public:
-    map<int, string, greater<>> d = {
-        {0, "Zero"}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"},
-        {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"},
-        {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"},
-        {18, "Eighteen"}, {19, "Nineteen"}, {20, "Twenty"}, {30, "Thirty"},
-        {40, "Forty"}, {50, "Fifty"}, {60, "Sixty"}, {70, "Seventy"},
-        {80, "Eighty"}, {90, "Ninety"}, {100, "Hundred"}, {1000, "Thousand"},
-        {1000000, "Million"}, {1000000000, "Billion"}
+    unordered_map<int, string> ones = {
+        {1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"},
+        {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}, {10,"Ten"},
+        {11,"Eleven"}, {12,"Twelve"}, {13,"Thirteen"}, {14,"Fourteen"},
+        {15,"Fifteen"}, {16,"Sixteen"}, {17,"Seventeen"}, {18,"Eighteen"},
+        {19,"Nineteen"}
     };
-    string numberToWords(int num) {
-        string res = "";
-        // cout << "get num " << num << endl;
 
-        if (num == 100) {
-            return "One Hundred";
+    unordered_map<int, string> tens = {
+        {2,"Twenty"}, {3,"Thirty"}, {4,"Forty"}, {5,"Fifty"},
+        {6,"Sixty"}, {7,"Seventy"}, {8,"Eighty"}, {9,"Ninety"}
+    };
+
+    vector<pair<int,string>> units = {
+        {1000000000,"Billion"}, {1000000,"Million"}, {1000,"Thousand"}
+    };
+
+    string numberToWords(int num) {
+        if (num == 0) return "Zero";
+        string res = "";
+
+        for (auto& [val, word] : units) {
+            if (num >= val) {
+                res += helper(num / val) + " " + word;
+                num = num % val;
+                if (num % val) {
+                    res += " ";
+                }
+            }
+        }
+        
+        if (num) {
+            res += helper(num);
         }
 
-        for (auto& entry : d) {
-            if (entry.first != 100 && entry.first != 1000 && entry.first != 1000000 && entry.first != 1000000000 && entry.first == num) {
-                res += entry.second;
-                break;
-            }
-            else if (entry.first != 0 && num / entry.first > 0) {
-                int multi = num / entry.first;
-                int remain = num % entry.first;
+        return res;
+    }
 
-                if (entry.first >= 100) {
-                    res += numberToWords(multi) + " ";
-                }
-                res += entry.second;
-                if (remain > 0) {
-                    res += " " + numberToWords(remain);
-                }
-                break; 
+    string helper(int num) {
+        if (num == 0) return "";
+
+        string res = "";
+
+        if (num < 20) {
+            res += ones[num];
+        }
+        else if (num >= 20 && num < 100) {
+            res += tens[num / 10];
+            if (num % 10) {
+                res += " " + helper(num % 10);
+            }
+        }
+        else {
+            res += ones[num / 100] + " Hundred";
+            if (num % 100) {
+                res += " " + helper(num % 100);
             }
         }
 
         return res;
     }
 };
-
-
