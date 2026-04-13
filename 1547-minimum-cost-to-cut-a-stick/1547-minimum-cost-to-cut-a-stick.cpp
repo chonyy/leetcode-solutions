43ecdb3575d5f7b5ef05693379ade873df588801
Tony Chou <tony.chou@intel.com>
2026-04-12 23:26:00 -0700

    Time: 27 ms (58.04%), Space: 13.9 MB (78.12%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
-        unordered_map<int,unordered_map<int, int>> cache;
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
 
-        return solve(0, n, cache, cuts);
+        int m = cuts.size();
+        vector<vector<int>> cache(m, vector<int>(m, -1));
+
+        return solve(0, m - 1, cache, cuts);
     }
 
-    int solve(int l, int r, unordered_map<int,unordered_map<int, int>>& cache, vector<int>& cuts) {
-        if (r - l <= 1) {
-            return 0;
-        }
-
-        if (cache.contains(l) && cache[l].contains(r)) {
-            return cache[l][r];
-        }
+    int solve(int l, int r, vector<vector<int>>& cache, vector<int>& cuts) {
+        if (r - l <= 1) return 0;
+        if (cache[l][r] != -1) return cache[l][r];
 
         int res = INT_MAX;
-
-        for (int c : cuts) {
-            if (c <= l || c >= r) {
-                continue;
-            }
-
-            int cost = solve(l, c, cache, cuts) + solve(c, r, cache, cuts);
-            res = min(res, cost);
-        }
-        
-        if (res == INT_MAX) {
-            cache[l][r] = 0;
-            return 0;
+        for (int i = l + 1; i < r; i++) {
+            res = min(res, solve(l, i, cache, cuts) + solve(i, r, cache, cuts));
         }
 
-        res += r - l;
-
-        cache[l][r] = res;
-        return res;
+        return cache[l][r] = res + cuts[r] - cuts[l];
     }
 };
