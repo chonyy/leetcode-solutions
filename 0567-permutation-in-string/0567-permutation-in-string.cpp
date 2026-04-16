a4d040866c293aeb3e3a864bb6ee0837478cbb16
Tony Chou <tony.chou@intel.com>
2026-04-15 18:10:59 -0700

    Time: 10 ms (31.99%), Space: 11.1 MB (15.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.size();
-        unordered_map<char,int> count1;
-        unordered_map<char,int> count2;
+        unordered_map<char,int> count;
+        for (char c : s1) {
+            count[c] ++;
+        }
+
+        int n = s2.size();
         int l = 0;
         int r = 0;
 
-        for (char c : s1) {
-            count1[c] ++;
-        }
-
-        while (r < s2.size()) {
-            count2[s2[r]] ++;
-
-            if (r - l + 1 == n) {
-                if(count1 == count2) {
-                    return true;
-                }
-
-                count2[s2[l]] --;
-                if(count2[s2[l]] == 0) {
-                    count2.erase(s2[l]);
+        while (r < n) {
+            if (r - l == s1.size()) {
+                count[s2[l]] ++;
+                if (count[s2[l]] == 0) {
+                    count.erase(s2[l]);
                 }
                 l ++;
+            }
+
+            count[s2[r]] --;
+            if (count[s2[r]] == 0) {
+                count.erase(s2[r]);
+            }
+
+            // for (auto& entry : count) {
+            //     cout << entry.first << " " << entry.second << endl;
+            // }
+            // cout << endl;
+
+            if (count.empty()) {
+                return true;
             }
 
             r ++;
