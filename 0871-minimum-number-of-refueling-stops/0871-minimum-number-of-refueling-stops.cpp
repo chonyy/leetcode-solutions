1b297eaa142e44086388ac3e8c56e8b0ab17912a
Tony Chou <tony.chou@intel.com>
2026-04-18 18:25:34 -0700

    Time: 0 ms (100%), Space: 20.1 MB (95.86%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        int n = stations.size();
+        int res = 0;
+        int cur = startFuel;
 
-        if (startFuel >= target) {
-            return 0;
-        }
+        // max heap
+        priority_queue<int> pq;
 
-        vector<vector<long long>> dp(n+1, vector<long long>(n+1, 0));
-        dp[0][0] = startFuel;
+        for (auto& station : stations) {
+            int pos = station[0];
+            int fuel = station[1];
 
-        for (int i = 1; i <= n; i ++) {
-            for (int j = 0; j <= i; j ++) {
-                if (j == 0) {
-                    dp[i][j] = startFuel;
-                    continue;
-                }
-
-                dp[i][j] = dp[i-1][j];
-
-                if (dp[i-1][j-1] >= stations[i-1][0]) {
-                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + stations[i-1][1]);
-                }
+            // add fuel until enough
+            while (!pq.empty() && cur < pos) {
+                cur += pq.top();
+                // cout << "add " << pq.top() << endl;
+                pq.pop();
+                res ++;
             }
+
+            if (pq.empty() && cur < pos) {
+                return -1;
+            }
+
+            pq.push(fuel);
         }
 
-        for (int j = 0; j <= n; j++) {
-            if (dp[n][j] >= target) return j;
+        // add fuel until enough
+        while (!pq.empty() && cur < target) {
+            cur += pq.top();
+            // cout << "add " << pq.top() << endl;
+            pq.pop();
+            res ++;
         }
 
-        return -1;
+        if (cur < target) {
+            return -1;
+        }
+
+        return res;
     }
 };
-
-// dp[i][j] = at ith stop, max reach with j stops
-
-// for i in n
-//     for j in 1 to i
-//         dp[i][j] = max(dp[i-1][j-1], dp[i][j-1] + );
-
-// res = first j that dp[i][j] >= target
-
-// [10,60],[20,30],[30,30],[60,40]
-
-// dp[i][0] = starFuel
-
-//    0  1  2  3  4
-// 0 10. 0  0. 0. 0
-// 1 10. 
-// 2 10
-// 3 10
-// 4 10
