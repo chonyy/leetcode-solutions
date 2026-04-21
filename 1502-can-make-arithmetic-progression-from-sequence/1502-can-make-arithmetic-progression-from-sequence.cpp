c7896d17feb9c729ebe7b87030fb4d72e8b15331
Tony Chou <tony.chou@intel.com>
2026-04-20 17:46:18 -0700

    Time: 0 ms (100%), Space: 12.7 MB (47.69%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -4,25 +4,33 @@ public:
         int localMin = INT_MAX;
         int localMax = INT_MIN;
         int n = arr.size();
-        unordered_set<int> seen;
 
         for (int num : arr) {
             localMin = min(localMin, num);
             localMax = max(localMax, num);
-            seen.insert(num);
         }
 
-        if (seen.size() != 1 && seen.size() != n) {
-            return false;
+        if (localMax - localMin == 0) {
+            return true;
         }
+
 
         int diff = localMax - localMin;
+        if (diff % (n-1) != 0) {
+            return false;
+        }
         int gap = diff / (n - 1);
 
-        for (int i = 0; i < n - 1; i ++) {
-            localMin += gap;
-            if (!seen.contains(localMin)) {
-                return false;
+        for (int i = 0; i < n; i ++) {
+            while (arr[i] != localMin + gap * i) {
+                if ((arr[i] - localMin) % gap != 0) {
+                    return false;
+                }
+                int idx = (arr[i] - localMin) / gap;
+                if (arr[i] == arr[idx]) {
+                    return false;
+                }
+                swap(arr[i], arr[idx]);
             }
         }
 
@@ -30,6 +38,6 @@ public:
     }
 };
 
-// 1 3 5
+// 1 4 5
 
 // diff = 4
