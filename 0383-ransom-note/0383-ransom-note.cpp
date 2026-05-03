5b806a3ffce02da97b493617938ab090329271b3
Tony Chou <tony.chou@intel.com>
2026-05-03 16:57:00 -0700

    Time: 0 ms (100%), Space: 12 MB (9.7%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> count;
+
+        for (char c : magazine) {
+            count[c] ++;
+        }
+
+        for (char c : ransomNote) {
+            count[c] --;
+            if (count[c] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
