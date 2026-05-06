aa3988ec672b3e891f103a7511ff82ce30c6598c
Tony Chou <tony.chou@intel.com>
2026-05-06 02:56:47 -0700

    Time: 215 ms (26.13%), Space: 55.7 MB (30.47%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        // minHeap
+        // push all first element to minHeap
+        // <value, listIdx, numIdx>
+        int k = nums.size();
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+
+        int maxNum = INT_MIN;
+        int minNum = INT_MAX;
+        for (int i = 0; i < k; i ++) {
+            pq.push({nums[i][0], i, 0});
+            maxNum = max(maxNum, nums[i][0]);
+            minNum = min(minNum, nums[i][0]);
+        }
+
+        int minRange = maxNum - minNum;
+        int resMin = minNum;
+        int resMax = maxNum;
+
+        // pop smallest from minHeap until end
+        while (pq.size() == k) {
+            auto& cur = pq.top();
+            int val = cur[0];
+            int listIdx = cur[1];
+            int numIdx = cur[2];
+            pq.pop();
+
+            // compare minRange and update
+            int range = maxNum - val;
+            if (range < minRange) {
+                minRange = range;
+                resMin = val;
+                resMax = maxNum;
+            }
+
+            // push new and update maxNum
+            if (numIdx < nums[listIdx].size() - 1) {
+                int newNum = nums[listIdx][numIdx + 1];
+                maxNum = max(maxNum, newNum);
+                pq.push({newNum, listIdx, numIdx + 1});
+            }
+        }
+
+        return {resMin, resMax};
+    }
+};
