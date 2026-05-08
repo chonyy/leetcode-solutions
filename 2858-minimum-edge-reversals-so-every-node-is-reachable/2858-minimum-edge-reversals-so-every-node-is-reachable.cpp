50f9467429c9cd66272b06a1a6034ba5ea308f40
Tony Chou <tony.chou@intel.com>
2026-05-07 21:35:50 -0700

    Time: 154 ms (83.24%), Space: 208.4 MB (96.28%) - LeetHub

--- 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
+    vector<vector<pair<int,int>>> graph;
+
     vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
-        unordered_map<int, unordered_map<int, int>> graph;
+        graph.resize(n);
 
         for (auto& e : edges) {
-            graph[e[0]][e[1]] = 0; // zero cost for normal edge
-            graph[e[1]][e[0]] = 1;
+            graph[e[0]].push_back({e[1], 0});
+            graph[e[1]].push_back({e[0], 1});
         }
 
-        int prev = visit(-1, 0, graph);
+        int prev = visit(-1, 0);
         vector<int> res(n);
-        
-        getAns(-1, 0, prev, graph, res);
+        getAns(-1, 0, prev, res);
 
         return res;
     }
 
-    void getAns(int parent, int node, int rev, unordered_map<int, unordered_map<int, int>>& graph, vector<int>& res) {
+    void getAns(int parent, int node, int rev, vector<int>& res) {
         res[node] = rev;
 
-        for (auto nei : graph[node]) {
-            if (nei.first == parent) {
-                continue;
-            }
-
-            // 1: hast cost, no edge, -1 to ans
-            // 0: no cost, is edge, +1 to ans
-            int newRev = rev + (nei.second == 1 ? -1 : 1);
-            getAns(node, nei.first, newRev, graph, res);
+        for (auto& [nei, cost] : graph[node]) {
+            if (nei == parent) continue;
+            getAns(node, nei, rev + (cost == 1 ? -1 : 1), res);
         }
     }
 
-    int visit(int parent, int node, unordered_map<int, unordered_map<int, int>>& graph) {
+    int visit(int parent, int node) {
         int reverse = 0;
 
-        auto& neighbors = graph[node];
-        for (auto nei : neighbors) {
-            if (nei.first == parent) {
-                continue;
-            }
-
-            reverse += nei.second;
-            reverse += visit(node, nei.first, graph);
+        for (auto& [nei, cost] : graph[node]) {
+            if (nei == parent) continue;
+            reverse += cost;
+            reverse += visit(node, nei);
         }
 
         return reverse;
