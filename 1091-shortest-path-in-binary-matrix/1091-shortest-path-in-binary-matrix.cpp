24e22eff4c7463b39f6009b5663c084877263a21
Tony Chou <tony.chou@intel.com>
2026-05-19 17:26:34 -0700

    Time: 14 ms (70.26%), Space: 25.5 MB (42.18%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+    vector<vector<int>> dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid.size();
-        
-        if(grid[0][0] == 1 || grid[rows-1][cols-1] == 1)
+        if (grid[0][0] == 1) {
             return -1;
-        
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        int distance = 1;
-        
-        while(!q.empty()) {
-            int size = q.size();
-            for(int i = 0; i < size; i ++) {
-                auto cur = q.front();
-                q.pop();
-                int row = cur.first;
-                int col = cur.second;
-                if(row == rows-1 && col == cols-1) {
-                    return distance;
-                }
-                
-                for(auto dir : directions) {
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];
-                    
-                    if(newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
-                        continue;
-                    }
-                    
-                    if(grid[newRow][newCol] == 1) {
-                        continue;
-                    }
-                    
-                    grid[newRow][newCol] = 1;
-                    q.push({newRow, newCol});
-                } 
-            }
-            distance ++;
         }
-        
+
+        vector<vector<int>> copy = grid;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        copy[0][0] = 1;
+        queue<pair<int,int>> q;
+        q.push({0, 0});
+
+        while (!q.empty()) {
+            auto cur = q.front();
+            int r = cur.first;
+            int c = cur.second;
+            q.pop();
+
+            if (r == rows - 1 && c == cols - 1) {
+                return copy[r][c];
+            }
+
+            for (auto& dir : dirs) {
+                int newR = r + dir[0];
+                int newC = c + dir[1];
+                int newCost = copy[r][c] + 1;
+
+                if (newR < 0 || newR >= rows || newC < 0 || newC >= cols) {
+                    continue;
+                }
+
+                if (copy[newR][newC] != 0) {
+                    continue;
+                }
+
+                copy[newR][newC] = newCost;
+                q.push({newR, newC});
+            }
+        }
+
         return -1;
     }
 };
