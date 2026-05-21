391b520f19a86bae8fbd9172c9b257460963aad0
Tony Chou <tony.chou@intel.com>
2026-05-21 01:08:50 -0700

    Time: 13 ms (39.92%), Space: 11.5 MB (41.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        // count t in map
+        // count remain
+        int remain = 0;
         unordered_map<char,int> count;
 
         for (char c : t) {
+            remain ++;
             count[c] ++;
         }
 
-        int r = 0;
         int l = 0;
+        int r = 0;
         int n = s.size();
-        int remain = t.size();
-        int resStart = -1;
-        int resLen = INT_MAX;
-
+        int ansStart = -1;
+        int ansLen = INT_MAX;
+        
+        // sliding window, moving r
         while (r < n) {
+            // cout << "r " << s[r] << endl;
+
+            // if map[r] > 0, remain --, map[r] --
             if (count[s[r]] > 0) {
                 remain --;
             }
             count[s[r]] --;
 
+            // while remain == 0
+            // - record ans
+            // - if map[l] >= 0, remain ++
+            // - map[l] ++
             while (remain == 0) {
+                // cout << "Move left " << s[l] << endl;
+                int curLen = r - l + 1;
+                if (curLen < ansLen) {
+                    ansLen = curLen;
+                    ansStart = l;
+                }
+
                 if (count[s[l]] >= 0) {
                     remain ++;
                 }
                 count[s[l]] ++;
-
-                int len = r - l + 1;
-                if (len < resLen) {
-                    resLen = len;
-                    resStart = l;
-                }
-
                 l ++;
             }
 
+            // cout << remain << endl;
+            // cout << endl;
             r ++;
         }
 
-        return resStart == -1 ? "" : s.substr(resStart, resLen);
+        return ansStart == -1 ? "" : s.substr(ansStart, ansLen);
     }
 };
-
-// ADOBECODEBANC
-//             r
-//          l 
-
-// A: 0
-// B: 1
-// C: 0
-// D: 0
-// O: 0
-// E: 0
-// N: -1
-
-// remain = 1
